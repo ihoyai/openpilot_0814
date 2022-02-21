07c24cf06adbca9410a4778073d4196a0f70c937
ihoyai <y2k2000bug@daum.net>
2022-02-21 22:45:47 +0900

    Revert "Revert "Revert "ss"""
    This reverts commit 2f90786dab06f2eb8c1cd1f32745a29b2fc53dc8.

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -11,8 +11,7 @@ def create_lkas11(packer, frame, car_fingerprint, apply_steer, steer_req,
                   left_lane, right_lane,
                   left_lane_depart, right_lane_depart, bus, ldws_opt):
   values = copy.copy(lkas11)
-    # values["CF_Lkas_LdwsSysState"] = sys_state
-  values["CF_Lkas_LdwsSysState"] = 3 if enabled else sys_state
+  values["CF_Lkas_LdwsSysState"] = sys_state
   values["CF_Lkas_SysWarning"] = 3 if sys_warning else 0
   values["CF_Lkas_LdwsLHWarning"] = left_lane_depart
   values["CF_Lkas_LdwsRHWarning"] = right_lane_depart
@@ -46,11 +45,10 @@ def create_lkas11(packer, frame, car_fingerprint, apply_steer, steer_req,
     values["CF_Lkas_LdwsActivemode"] = 2
     values["CF_Lkas_SysWarning"] = lkas11["CF_Lkas_SysWarning"]
 
-  elif car_fingerprint == CAR.K7:
-    values["CF_Lkas_LdwsActivemode"] = 2
-    #values["CF_Lkas_SysWarning"] = 4 if sys_warning else 0
-    values["CF_Lkas_FcwOpt_USM"] = 1
-    values["CF_Lkas_LdwsOpt_USM"] = 3
+  elif car_fingerprint == CAR.SONATA_LF_TURBO:
+    values["CF_Lkas_LdwsOpt_USM"] = 2
+    values["CF_Lkas_FcwOpt_USM"] = 2 if enabled else 1
+    values["CF_Lkas_SysWarning"] = 4 if sys_warning else 0
 
   if ldws_opt:
     values["CF_Lkas_LdwsOpt_USM"] = 3
